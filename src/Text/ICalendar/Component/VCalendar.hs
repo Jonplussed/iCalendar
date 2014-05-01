72b364746eca276aa9b556f233ca292b5f5e0c40
Jon Childress <jon@childr.es>
2014-04-30 21:10:29 -0500

    added vEvents easy as pie!

--- src/Text/ICalendar/Component/VCalendar.hs
+++ src/Text/ICalendar/Component/VCalendar.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Text.ICalendar.Component.VCalendar
 ( VCalendar(..)
@@ -9,19 +9,20 @@ import Text.Parsec.Error
 
 import Text.ICalendar.Parser.Combinators
 import Text.ICalendar.Parser.Validators
---import Text.ICalendar.Component.VEvent ( VEvent, vEvent )
+import Text.ICalendar.Component.VEvent
 
 data VCalendar = VCalendar { prodid   :: String
                            , version  :: String
                            , calscale :: Maybe String
                            , method   :: Maybe String
-                           --, vEvents  :: [VEvent]
+                           , vEvents  :: [VEvent]
                            } deriving (Eq, Show)
 
 vCalendar :: ICalTree -> Either ParseError VCalendar
 vCalendar tree = do
-  prodid    <- reqProp1 "PRODID"    tree
-  version   <- reqProp1 "VERSION"   tree
-  calscale  <- optProp1 "CALSCALE"  tree
-  method    <- optProp1 "METHOD"    tree
+  prodid    <- reqProp1 "PRODID"        tree
+  version   <- reqProp1 "VERSION"       tree
+  calscale  <- optProp1 "CALSCALE"      tree
+  method    <- optProp1 "METHOD"        tree
+  vEvents   <- optCompN "VEVENT" vEvent tree
   return VCalendar {..}
--- src/Text/ICalendar/Component/VEvent.hs
+++ src/Text/ICalendar/Component/VEvent.hs
@@ -1,53 +1,35 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Text.ICalendar.Component.VEvent
 ( VEvent(..)
 , vEvent
 ) where
 
-import Text.ParserCombinators.Parsec
-import Text.ICalendar.Parsec.Combinators
+import Text.Parsec.Error
 
-data VEvent = VEvent { attendee    :: String
-                     , description :: String
-                     , dtstart     :: String
-                     , duration    :: String
-                     , location    :: String
-                     , organizer   :: String
-                     , summary     :: String
-                     , transp      :: Transparency
-                     , uid         :: String
-                     , eWarnings    :: [String] } deriving (Eq, Show)
+import Text.ICalendar.Parser.Combinators
+import Text.ICalendar.Parser.Validators
 
-data Transparency = Transparent
-                  | Opaque
-                  deriving (Eq, Show)
+data VEvent = VEvent { dtstart      :: String
+                     , attendee     :: [String]
+                     , uid          :: Maybe String
+                     , duration     :: Maybe String
+                     , organizer    :: Maybe String
+                     , location     :: Maybe String
+                     , summary      :: Maybe String
+                     , description  :: Maybe String
+                     , transp       :: Maybe String
+                     } deriving (Eq, Show)
 
-vEvent :: Parser VEvent
-vEvent = assignAttrs empty attributes
-
-empty :: VEvent
-empty = VEvent { attendee     = ""
-               , description  = ""
-               , dtstart      = ""
-               , duration     = ""
-               , location     = ""
-               , organizer    = ""
-               , summary      = ""
-               , transp       = Opaque
-               , uid          = ""
-               , eWarnings     = []
-               }
-
-attributes :: [Parser (VEvent -> VEvent)]
-attributes = [ property "attendee"    $ \v e -> e { attendee = v }
-             , property "description" $ \v e -> e { description = v }
-             , property "dtstart"     $ \v e -> e { dtstart = v }
-             , property "duration"    $ \v e -> e { duration = v }
-             , property "location"    $ \v e -> e { location = v }
-             , property "organizer"   $ \v e -> e { organizer = v }
-             , property "summary"     $ \v e -> e { summary = v }
-             , property "transp"      $ \v e -> e { transp = transparency v }
-             , property "uid"         $ \v e -> e { uid = v }
-             ]
-
-transparency :: String -> Transparency
-transparency t = if t == "TRANSPARENT" then Transparent else Opaque
+vEvent :: ICalTree -> Either ParseError VEvent
+vEvent tree = do
+  dtstart     <- reqProp1 "DTSTART"     tree
+  attendee    <- optPropN "ATTENDEE"    tree
+  uid         <- optProp1 "UID"         tree
+  duration    <- optProp1 "DURATION"    tree
+  organizer   <- optProp1 "ORGANIZER"   tree
+  location    <- optProp1 "LOCATION"    tree
+  summary     <- optProp1 "SUMMARY"     tree
+  description <- optProp1 "DESCRIPTION" tree
+  transp      <- optProp1 "TRANSP"      tree
+  return VEvent {..}
